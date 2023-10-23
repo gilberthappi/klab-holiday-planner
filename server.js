b3833aec56d93d34ed16f03c78952f6eb75abe6b
gilberthappi <gdushimimana6@gmail.com>
2023-10-23 17:28:30 +0200

    Adding Tour swagger

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
 import mainRouter from './src/routes';
-// import { log } from 'console';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
 require('dotenv').config();
 
@@ -18,13 +18,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1', mainRouter);
 
-app.get("/",(req,res)=>{
- res.status(200).json({message:"Welcome to the API"})
+// Define the Swagger options and generate the Swagger specification
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Holiday Tours API',
+      version: '1.0.0',
+      description: 'Holiday Tours API Documentation',
+    },
+   servers: [
+  {
+    url: 'http://localhost:1500/api/v1', // Adjust the port to match your configuration
+  },
+  {
+    url: 'https://holiday-api-zj3a.onrender.com/api/v1', // Adjust the port to match your configuration
+  },
+],
+  },
+  apis: ['./src/routes/*.js'], // Use your actual API route files here
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+// Serve Swagger UI at /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the API' });
 });
+
 mongoose.connect(process.env.DB_CONNECTION_PROD).then(() => {
   console.log('Database is connected');
 });
 
-app.listen(port, () => {
+app. listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
 });
--- src/routes/Tours.js
+++ src/routes/Tours.js
@@ -10,12 +10,313 @@ const tourRouter = express.Router();
 
 tourRouter.get('/all', getTours);
 
+tourRouter.get('/getByField', getTours);
+
 tourRouter.post('/addNew',  uploaded, createTour);
 
 tourRouter.post('/addNews', uploaded, createTours);
 
-tourRouter.put('/update', updateTour);
+tourRouter.put('/update', uploaded, updateTour);
 
 tourRouter.delete('/delete', verifyToken, deleteTours);
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *      bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *   schemas:
+ *     Tour:
+ *       type: object
+ *       properties:
+ *         destination:
+ *           type: string
+ *         backdropImage:
+ *           type: string
+ *         title:
+ *           type: string
+ *         Description:
+ *           type: string
+ *         Duration:
+ *           type: string
+ *         Group_size:
+ *           type: string
+ *         Price:
+ *           type: string
+ *         Discount:
+ *           type: string
+ *         Tour_type:
+ *           type: string
+ *         Departure:
+ *           type: string
+ *         Seats:
+ *           type: integer
+ *         fromMonth:
+ *           type: string
+ *         toMonth:
+ *           type: string
+ *         departureTime:
+ *           type: string
+ *         ReturnTime:
+ *           type: string
+ *         Gallery:
+ *           type: array
+ *           items:
+ *             type: string
+ */
+/**
+ * @swagger
+ * /tour/all:
+ *   get:
+ *     summary: Get all tours
+ *     tags: [Tours]
+ *     description: Get a list of all tours.
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Tour'
+ */
+
+
+/**
+ * @swagger
+ * /tour/getByField:
+ *   get:
+ *     summary: Get a tour by a specified 
+ *     tags: [Tours]
+ *     description: Get a single tour by a specified field (e.g., "_id" or "destination").
+ *     parameters:
+ *       - in: query
+ *         name: fieldName
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The field name to search for (e.g., "_id" or "destination").
+ *       - in: query
+ *         name: value
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The value to match in the specified field.
+ *     responses:
+ *       200:
+ *         description: Tour found successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Tour'
+ *       404:
+ *         description: Tour not found
+ * */
+
+
+
+/**
+ * @swagger
+ * tags:
+ *  name: Tours
+ * description: Tours API
+ * 
+ */ 
+
+/**
+ * @swagger
+ * /tour/addNew:
+ *  post:
+ *    summary: -Create a new tour
+ *    tags: [Tours]
+ *    description: Create a new tour
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        multipart/form-data:
+ *          schema:
+ *            type: object
+ *            properties:
+ *             destination:
+ *              type: string
+ *             backdropImage:
+ *              type: file
+ *              items:
+ *                type: String
+ *                format: binary
+ *             title:
+ *              type: string
+ *             Description:
+ *              type: string
+ *             Duration:
+ *              type: string
+ *             Group_size:
+ *              type: string
+ *             Price:
+ *              type: string
+ *             Discount:
+ *              type: string
+ *             Tour_type:
+ *              type: string
+ *             Departure:
+ *              type: string
+ *             Seats:
+ *              type: integer
+ *             fromMonth:
+ *              type: string
+ *             toMonth:
+ *              type: string
+ *             departureTime:
+ *              type: string
+ *             ReturnTime:
+ *              type: string
+ *             Gallery:
+ *              type: array
+ *              items:
+ *                type: file
+ *                format: binary
+ *    responses:
+ *       201:
+ *         description: Tour created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Tour'
+ *       404:
+ *         description: Failed to add tour
+ */
+
+
+
+/**
+ * @swagger
+ * /tour/update:
+ *   put:
+ *     summary: Update a tour
+ *     tags: [Tours]
+ *     description: Update an existing tour.
+ *     parameters:
+ *       - in: query
+ *         name: fieldName
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The field name to identify the tour to update (e.g., "_id" or "destination").
+ *       - in: query
+ *         name: value
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The value of the field to match when identifying the tour.
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        multipart/form-data:
+ *          schema:
+ *            type: object
+ *            properties:
+ *             destination:
+ *              type: string
+ *             backdropImage:
+ *              type: file
+ *              items:
+ *                type: String
+ *                format: binary
+ *             title:
+ *              type: string
+ *             Description:
+ *              type: string
+ *             Duration:
+ *              type: string
+ *             Group_size:
+ *              type: string
+ *             Price:
+ *              type: string
+ *             Discount:
+ *              type: string
+ *             Tour_type:
+ *              type: string
+ *             Departure:
+ *              type: string
+ *             Seats:
+ *              type: integer
+ *             fromMonth:
+ *              type: string
+ *             toMonth:
+ *              type: string
+ *             departureTime:
+ *              type: string
+ *             ReturnTime:
+ *              type: string
+ *             Gallery:
+ *              type: array
+ *              items:
+ *                type: file
+ *                format: binary
+ *     responses:
+ *       200:
+ *         description: Tour updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Tour'
+ *       404:
+ *         description: Tour not found or update failed
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
+
+
+
+/**
+ * @swagger
+ * /tour/delete:
+ *   delete:
+ *     summary: Delete a tour
+ *     tags: [Tours]
+ *     security:
+ *       - bearerAuth: []
+ *     description: Delete an existing tour.
+ *     parameters:
+ *       - in: query
+ *         name: fieldName
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The field name to identify the tour to delete (e.g., "_id" or "destination").
+ *       - in: query
+ *         name: value
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The value of the field to match when identifying the tour.
+ *     responses:
+ *       200:
+ *         description: Tour deleted successfully
+ *       404:
+ *         description: Tour not found or delete failed
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
+
+
+
+
+
+
 
 export default tourRouter;
