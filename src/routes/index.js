575abdcee325d35842f67657dc01281ab86dbec8
gilberthappi <gdushimimana6@gmail.com>
2023-10-24 10:44:48 +0200

    try to fix deployment

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRouter from './authentication';
 import tourRouter from './Tours';
+import bookingRouter from './booking';
 
 const mainRouter = express.Router();
 
 mainRouter.use('/auth', authRouter);
 mainRouter.use('/tour', tourRouter);
+mainRouter.use('/booking', bookingRouter)
 
 export default mainRouter;
