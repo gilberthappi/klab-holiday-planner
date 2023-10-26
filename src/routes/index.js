b303182a0e6dd6f621a8cbb03da55453de32c68e
gilberthappi <gdushimimana6@gmail.com>
2023-10-26 11:26:03 +0200

    Update index model,routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import authRouter from './authentication';
 import tourRouter from './Tours';
 import bookingRouter from './booking';
+import contactRouter from './contacts';
 
 const mainRouter = express.Router();
 
 mainRouter.use('/auth', authRouter);
 mainRouter.use('/tour', tourRouter);
-mainRouter.use('/booking', bookingRouter)
+mainRouter.use('/booking', bookingRouter);
+mainRouter.use('/cont', contactRouter);
 
 export default mainRouter;
